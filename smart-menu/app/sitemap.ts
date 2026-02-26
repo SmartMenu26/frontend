1ed27b5a3325e3bdc4c6acae1d4880e9a3e3e22f
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2026-02-26 14:44:14 +0100

    add sitemap file

--- smart-menu/app/sitemap.ts
+++ smart-menu/app/sitemap.ts
@@ -1,10 +1,18 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.smartmenumk.com',
+      url: "https://www.smartmenumk.com",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
-  ]
+    {
+      url: "https://www.smartmenumk.com/mk",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
 }
