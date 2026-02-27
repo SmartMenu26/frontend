add7e6b6e376d72972e13c475d0977018f99ca91
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2026-02-27 14:50:00 +0100

    remove not used fonts

--- smart-menu/app/layout.tsx
+++ smart-menu/app/layout.tsx
@@ -1,23 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import RegisterSW from "./_components/RegisterSW";
-import { Rubik, Birthstone, Inter, Great_Vibes } from "next/font/google";
+import { Inter, Great_Vibes } from "next/font/google";
 import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
-export const rubik = Rubik({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "700", "900"],
-  variable: "--font-rubik",
-  display: "swap",
-});
-
-export const birthstone = Birthstone({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-birthstone",
-  display: "swap",
-});
 
 export const great_vibes = Great_Vibes({
   weight: "400",
@@ -103,7 +89,7 @@ export default function RootLayout({
             gtag('config', 'G-SX57E0RGSR');
           `}
       </Script>
-      <body className={`${inter.variable} ${rubik.variable} ${birthstone.variable} ${great_vibes.variable}`}>
+      <body className={`${inter.variable} ${great_vibes.variable}`}>
         <RegisterSW />
         {children}
         <SpeedInsights />
