fab9b363998f2012baa956488b5ad559fab19d7e
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2026-03-19 15:57:59 +0100

    update env

--- smart-menu/app/[locale]/page.tsx
+++ smart-menu/app/[locale]/page.tsx
@@ -66,7 +66,7 @@ type RestaurantRecord = {
 async function loadRestaurants(): Promise<RestaurantRecord[]> {
   const origin =
     process.env.NEXT_PUBLIC_APP_URL ||
-    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "") ||
+    process.env.NEXT_PUBLIC_SITE_URL ||
     "http://localhost:3000";
 
   try {
